bca2c6fe02e123c0eb16ab68205b5f46a9efeb04
Alex Weber <alex.weber212@gmail.com>
2016-05-02 17:34:42 -0400

    Added vampire detection logic

---
+++ ruby/vampires.rb
@@ -0,0 +1,26 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.to_i
+
+puts "What year were you born?"
+year_born = gets.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+garlic_bread = gets.chomp
+garlic_bread.downcase!
+
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
+insurance = gets.chomp
+insurance.downcase!
+
+if name == "Drake Cula" || name == "Tu Fang"
+	puts "Definitely a vampire."
+elsif "#{2016 - age}" == "#{year_born}" && garlic_bread == "yes" && insurance == "yes"
+	puts "Probably not a vampire."
+elsif "#{2016 - age}" != "#{year_born}" && (garlic_bread == "no" || insurance == "no")
+	puts "Probably a vampire."
+else
+	puts "Results inconclusive."
+end
