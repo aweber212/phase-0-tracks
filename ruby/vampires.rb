61661f661ca4371c144c8e16daf89cddfd047838
Alex Weber <alex.weber212@gmail.com>
2016-05-02 17:56:34 -0400

    Added employee countdown loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,26 +1,35 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.to_i
 
-puts "How old are you?"
-age = gets.to_i
+until employees == 0
 
-puts "What year were you born?"
-year_born = gets.to_i
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-garlic_bread = gets.chomp
-garlic_bread.downcase!
+	puts "How old are you?"
+	age = gets.to_i
 
-puts "Would you like to enroll in the company's health insurance? (yes/no)"
-insurance = gets.chomp
-insurance.downcase!
+	puts "What year were you born?"
+	year_born = gets.to_i
 
-if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire."
-elsif "#{2016 - age}" == "#{year_born}" && garlic_bread == "yes" && insurance == "yes"
-	puts "Probably not a vampire."
-elsif "#{2016 - age}" != "#{year_born}" && (garlic_bread == "no" || insurance == "no")
-	puts "Probably a vampire."
-else
-	puts "Results inconclusive."
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+	garlic_bread = gets.chomp
+	garlic_bread.downcase!
+
+	puts "Would you like to enroll in the company's health insurance? (yes/no)"
+	insurance = gets.chomp
+	insurance.downcase!
+
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire."
+	elsif "#{2016 - age}" == "#{year_born}" && garlic_bread == "yes" && insurance == "yes"
+		puts "Probably not a vampire."
+	elsif "#{2016 - age}" != "#{year_born}" && (garlic_bread == "no" || insurance == "no")
+		puts "Probably a vampire."
+	else
+		puts "Results inconclusive."
+	end
+	employees -= 1
 end
+
+puts "Thank you for using our services!"
