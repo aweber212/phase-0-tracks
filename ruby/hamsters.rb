cf42889dc50819dbeccc161b6c15b09955d24928
Alex Weber <alex.weber212@gmail.com>
2016-05-03 14:49:58 -0400

    Added hamster file

---
+++ ruby/hamsters.rb
@@ -0,0 +1,24 @@
+puts "What is your hamster's name?"
+name = gets.chomp
+
+puts "On a scale of 1 to 10, how loud is #{name}?"
+volume = gets.to_i
+
+puts "What color is #{name}'s fur?"
+fur = gets.chomp
+
+puts "Is #{name} a good candidate for adoption?"
+candidate = gets.chomp
+
+puts "What is #{name}'s estimated age?"
+age = gets.to_i
+
+if age = ""
+	age = nil
+end
+
+puts "The next hamster arriving is #{name}!" 
+puts "#{name}'s fur color is #{fur}." 
+puts "On a volume scale of 1 to 10, #{name} is a #{volume}." 
+puts "#{name} is #{age} years old."
+puts "Is #{name} a good candidate for adoption? #{candidate}."
