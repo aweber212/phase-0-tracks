6e85599ec6013ce74a15a379f70523732060d71f
Alex Weber <alex.weber212@gmail.com>
2016-05-16 23:54:34 -0400

    Added outline for Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+SPECIES ------------------------
+Mansbestfriendus Thatpuppyissocuteisus
+
+CHARACTERISTICS ----------------
+Shed coat frequency: varies
+Prone to lick face: yes
+Barks per hour: 14
+Size: varies
+Fur color: varies
+
+BEHAVIOR -----------------------
+Bark
+Jumping on human
+Play cute for food
