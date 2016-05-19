a8aa173d888d2ae1165f30426b2eb0d26ad45bbc
Alex Weber <alex.weber212@gmail.com>
2016-05-19 15:56:09 -0400

    Added code to santa.rb

---
+++ ruby/santa.rb
@@ -0,0 +1,50 @@
+class Santa
+
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+
+	def initialize(gender, ethnicity, age = 0)
+		p "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = age
+	end
+
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		p "That was a good #{cookie}!"
+	end
+
+	def celebrate_birthday
+		@age = age + 1
+	end
+
+	#def get_mad_at(reindeer)
+	#	@reindeer_ranking = @reindeer_ranking(6, array.delete_at(3))
+	#end
+
+end
+
+st_nick = Santa.new("male", "white")
+
+st_nick.speak
+st_nick.eat_milk_and_cookies("oreo")
+st_nick.celebrate_birthday
+st_nick.gender = "N/A"
+p st_nick.ethnicity
+p st_nick.age
+
+p st_nick
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_age = [*1...140]
+
+100.times do |i|
+	  p Santa.new(example_genders.shuffle[i], example_ethnicities.shuffle[i], example_age.shuffle[i])
+	  
+end
