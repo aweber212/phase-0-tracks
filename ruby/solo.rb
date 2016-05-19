613ed5a42c28f68db9aec7f47d8218edb3da74cd
Alex Weber <alex.weber212@gmail.com>
2016-05-19 18:04:51 -0400

    Initial commit /w design

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+# Create bond class
+# Initialiaze method
+	# Define attributes
+		# bad guys neutralized (integer)
+		# arch enemy (string)
+		# drinken a martini yet (boolean)
+	# Define Methods
+		# tell world what his name is
+		# add to bad guys neutralized attribute
+		# drink a martini
+
