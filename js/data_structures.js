c0dff2059996ec1728b3bbd4b32e176df80f05aa
Alex Weber <alex.weber212@gmail.com>
2016-05-30 19:09:59 -0400

    Added car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,9 +8,46 @@ names.push("Aristrocrat")
 for (var i = 0; i < colors.length; i++) {
 	var horse = {}
 	horse[names[i]] = colors[i];
-	console.log(horse)
+	console.log(horse);
 }
 
+console.log("-----")
 
+function Car(brand, model, color, isElectric) {
 
-console.log(horse)
+	this.brand = brand;
+	this.model = model
+	this.color = color
+	this.isElectric = isElectric
+	this.vroom = function () { console.log("VROOOOOOOOOM!!!"); };
+
+}
+
+var Camry = new Car("Toyota", "Camry", "Silver", false);
+console.log("This is a " + Camry.color + " " + Camry.brand + " " + Camry.model)
+	if (this.isElectric) {
+		console.log("It is Electric")
+	} else {
+		console.log("It is not Electric")
+	}
+Camry.vroom()
+console.log("-----")
+
+var Volt = new Car("Chevy", "Volt", "Metallic Blue", true);
+console.log("This is a " + Volt.color + " " + Volt.brand + " " + Volt.model)
+	if (this.isElectric) {
+		console.log("It is Electric")
+	} else {
+		console.log("It is not Electric")
+	}
+Volt.vroom()
+console.log("-----")
+
+var Lambo = new Car("Lamborghini", "Aventador", "Fire Red", false)
+console.log("This is a " + Lambo.color + " " + Lambo.brand + " " + Lambo.model)
+	if (this.isElectric) {
+		console.log("It is Electric")
+	} else {
+		console.log("It is not Electric")
+	}
+Lambo.vroom()
