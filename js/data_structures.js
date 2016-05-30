6677f58c498b2bc4f0be1faaac1c944e19556924
Alex Weber <alex.weber212@gmail.com>
2016-05-30 18:40:04 -0400

    Added arrays and objects

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,4 +1,16 @@
 var colors = ["Green", "Blue", "Red", "Gold"]
 var names = ["Ed", "Lundqvist", "The Colonel", "Sparta"]
 
+colors.push("Velvet")
 names.push("Aristrocrat")
+
+
+for (var i = 0; i < colors.length; i++) {
+	var horse = {}
+	horse[names[i]] = colors[i];
+	console.log(horse)
+}
+
+
+
+console.log(horse)
